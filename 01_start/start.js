e3dace24a5a5fbc6d797046a3db8e928de790cac
Tyler <nguyentyler97@gmail.com>
2022-10-19 16:34:36 -0700

    feat: Add title & change animation transitions

--- 01_start/start.js
+++ 01_start/start.js
@@ -2,9 +2,11 @@
     const redirect = () => {
         window.location = "https://www.google.com/"
     }
+    const title = document.getElementById('title')
     const button = document.getElementById('startButton')
     button.addEventListener('click', () => {
-        button.classList.add('animated')
+        button.classList.add('buttonAnimated')
+        title.classList.add('titleAnimated')
         setTimeout(redirect, 1000)
     })
 })()
