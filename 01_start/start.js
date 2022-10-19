1f7d57f9607ae12a5ec83c28468ef13406a6c6eb
Tyler <nguyentyler97@gmail.com>
2022-10-19 16:14:57 -0700

    feat: Add name files & add redirect url function to button

--- 01_start/start.js
+++ 01_start/start.js
@@ -1,4 +1,10 @@
-const button = document.getElementById('startButton')
-button.addEventListener('click', () => {
-    button.classList.add('animated')
-})
+(function () {
+    const redirect = () => {
+        window.location = "https://www.google.com/"
+    }
+    const button = document.getElementById('startButton')
+    button.addEventListener('click', () => {
+        button.classList.add('animated')
+        setTimeout(redirect, 1000)
+    })
+})()
