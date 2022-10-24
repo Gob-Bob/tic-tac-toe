528494f70531ae27f5ef181215f669dcec032df3
Tyler <nguyentyler97@gmail.com>
2022-10-24 12:15:21 -0700

    feat: change player 1 codes to player 2 with new animation

--- 03_player_2/player_2.js
+++ 03_player_2/player_2.js
@@ -1,6 +1,6 @@
 (function () {
     const redirect = () => {
-        window.location = "http://127.0.0.1:5500/tic-tac-toe/03_player_2/player_2.html"
+        window.location = "https://github.com/Gob-Bob/tic-tac-toe"
     }
     const form = document.getElementById('playerForm')
     const button = document.getElementById('submitButton')
