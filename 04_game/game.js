197c029c9a689897516e9fbee1d0d00f274502dc
Tyler <nguyentyler97@gmail.com>
2022-11-07 07:46:16 -0800

    fix: properly render the board

--- 04_game/game.js
+++ 04_game/game.js
@@ -15,18 +15,21 @@ const gameBoardMod = (function() {
     const playerOneDisplayName = document.getElementById('player-one-display-name')
     const playerOneForm = document.getElementById('player-one-form')
     const playerOneButton = document.getElementById('player-one-submit-button')
+    playerOneDisplayName.textContent = '^'
     playerOneButton.addEventListener('click', () => {
         playerOneForm.classList.add('animated')
         playerOneDisplayName.textContent = playerOneName.value
         playerOneDisplayName.classList.add('second-wrapper-animated')
         playerOne = playerTemplate(1, playerOneName.value)
     })
+
     // Set up player 2
     let playerTwo = playerTemplate(2)
     const playerTwoName = document.getElementById('player-two-name')
     const playerTwoDisplayName = document.getElementById('player-two-display-name')
     const playerTwoForm = document.getElementById('player-two-form')
     const playerTwoButton = document.getElementById('player-two-submit-button')
+    playerTwoDisplayName.textContent = '^'
     playerTwoButton.addEventListener('click', () => {
         playerTwoForm.classList.add('animated')
         playerTwoDisplayName.textContent = playerTwoName.value
@@ -44,59 +47,50 @@ const gameBoardMod = (function() {
     }
 
     const boardDOM = document.getElementById('game_grid_container')
-    // const render = (function(array) {
-    //     array.forEach(item => {
-            
-    //         // Clear the DOM of the old board (probably set array to empty first?)
-            
-    //         const boardSlot = document.createElement('button')
-    //         boardSlot.textContent = item
-    //         boardSlot.classList.add('game_slot')
-    //         boardDOM.appendChild(boardSlot)
-    //     })
-    // })(boardObj.boardArray)
 
     const removeChildNodes = (parent) => {
         while (parent.firstChild) {
             parent.removeChild(parent.firstChild)
         }
     }
-
+    
+    const board = boardObj.boardArray
     const render = () => {
-        board = boardObj.boardArray
+
         removeChildNodes(boardDOM)
         board.forEach(element => {
-            const boardSlot = document.createElement('button')
-            boardSlot.textContent = element
-            boardSlot.classList.add('game_slot')
-            boardDOM.appendChild(boardSlot)
+            const button = document.createElement('button')
+            button.textContent = element
+            button.classList.add('game_slot')
+            boardDOM.appendChild(button)
+
         })
+
+        const gameSlots = document.querySelectorAll('.game_slot')
+        gameSlots.forEach((slot, index) => {
+            slot.addEventListener('click', () => {
+                playerPlaceMove(boardObj.boardArray, index, count)
+                // Access the gameboard method to check for a win
+            })
+        })
+
     }
     render()
 
     let count = 0
-    const getPlayerSymbol = () => {
-        if (count % 2 == 0) {
+    const getPlayerSymbol = (num) => {
+        if (num % 2 == 0) {
             return playerOne.playerSym
         } else {
             return playerTwo.playerSym
         }
     }
 
-    const playerPlaceMove = (array, index) => {
+    const playerPlaceMove = (array, index, num) => {
         if (array[index] == '') {
-            array[index] = getPlayerSymbol()
+            array[index] = getPlayerSymbol(num)
         }
         render()
         count++
     }
-            
-    const gameSlots = document.querySelectorAll('.game_slot')
-    gameSlots.forEach((slot, index) => {
-        slot.addEventListener('click', () => {
-            playerPlaceMove(boardObj.boardArray, index)
-            // Access the gameboard method to check for a win
-            // Rerender the board to update moves onto the DOM (renderBoard function)
-        })
-    })
 })()
