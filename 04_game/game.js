daf76fcc0ef475f78a70a33af0b1bde6a8033b31
Tyler <nguyentyler97@gmail.com>
2022-11-06 18:57:58 -0800

    feat: allow players to mark the board

--- 04_game/game.js
+++ 04_game/game.js
@@ -10,6 +10,7 @@ const playerTemplate = (playerNum, playerName) => {
 const gameBoardMod = (function() {
     
     // Set up player 1
+    let playerOne = playerTemplate(1)
     const playerOneName = document.getElementById('player-one-name')
     const playerOneDisplayName = document.getElementById('player-one-display-name')
     const playerOneForm = document.getElementById('player-one-form')
@@ -18,10 +19,10 @@ const gameBoardMod = (function() {
         playerOneForm.classList.add('animated')
         playerOneDisplayName.textContent = playerOneName.value
         playerOneDisplayName.classList.add('second-wrapper-animated')
-        const playerOne = playerTemplate(1, playerOneName.value)
-        console.log(playerOne.playerSym)
+        playerOne = playerTemplate(1, playerOneName.value)
     })
     // Set up player 2
+    let playerTwo = playerTemplate(2)
     const playerTwoName = document.getElementById('player-two-name')
     const playerTwoDisplayName = document.getElementById('player-two-display-name')
     const playerTwoForm = document.getElementById('player-two-form')
@@ -30,8 +31,7 @@ const gameBoardMod = (function() {
         playerTwoForm.classList.add('animated')
         playerTwoDisplayName.textContent = playerTwoName.value
         playerTwoDisplayName.classList.add('second-wrapper-animated')
-        const playerTwo = playerTemplate(2, playerTwoName.value)
-        console.log(playerTwo.playerSym)
+        playerTwo = playerTemplate(2, playerTwoName.value)
     })
 
     const boardObj = {
@@ -45,9 +45,6 @@ const gameBoardMod = (function() {
 
     const boardDOM = document.getElementById('game_grid_container')
     const renderBoard = (function(array) {
-        // For each item in the board, 
-            // Create a button and insert the name of the element into the button's textContent
-            // Add it to the button wrapper HTML element
         array.forEach(item => {
             const boardSlot = document.createElement('button')
             boardSlot.textContent = item
@@ -58,43 +55,27 @@ const gameBoardMod = (function() {
 
     let count = 0
     const getPlayerTurn = () => {
-        // Alternates between even and odd to determine playerOne or playerTwo's turn
         if (count % 2 == 0) {
-            return 1
+            return playerOne.playerSym
         } else {
-            return 2
+            return playerTwo.playerSym
         }
     }
-    
-    const getPlayerName = (playerCounterInput) => {
-        // Check getPlayerTurn
-        // Return player's name property depending if even or odd
-    }
-
-    // getPlayerSymbol function
-        // Check getPlayerTurn
-        // Return player's symbol property depending if even or odd
-    
-    // playerPlaceMove function (playerSymbol, positionIndex, array)
-        // Place playerSymbol into game spot using positionIndex
-        // Place within board array
 
     const playerPlaceMove = (slot, position) => {
-        // If position is blank, place player symbol
-        if (slot == '') {
-            console.log(playerOne.playerSym)
-            // Switch to next player's turn
+        if (slot.textContent == '') {
+            slot.textContent = getPlayerTurn()
+            // Function adds player's symbol into appropriate spot in array
+            // Clear the DOM of the old board
+            // Run board render function to update
             count++
         }
-        // Otherwise don't do anything and allow the player to place in diferent spot
-        
     }
             
     const gameSlots = document.querySelectorAll('.game_slot')
     gameSlots.forEach((slot, position) => {
-        let content = slot.textContent
         slot.addEventListener('click', () => {
-            playerPlaceMove(content, position)
+            playerPlaceMove(slot, position)
             // Access the gameboard method to check for a win
             // Rerender the board to update moves onto the DOM (renderBoard function)
         })
